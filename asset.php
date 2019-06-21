6abab2ef51678f750df779d5d09fc3a8aba3009b
Anderson Ismael <23392311+aicouto@users.noreply.github.com>
2019-06-21 09:08:04 -0300

    A  asset.php

---
+++ asset.php
@@ -0,0 +1,31 @@
+<?php
+//Criado por Anderson Ismael
+//21 de junho de 2019
+
+function asset($urls){
+    if(is_string($urls)){
+        $arr[]=$urls;
+        $urls=$arr;
+    }
+    foreach ($urls as $url) {
+        $filename=ROOT.'public'.$url;
+        $filename2=ROOT.$url;
+        $path_parts = pathinfo($url);
+        $ext=$path_parts['extension'];
+        if(file_exists($filename)){
+            $md5=md5_file($filename);
+            $url=$url."?$md5";
+        }elseif(file_exists($filename2)){
+            $md5=md5_file($filename2);
+            $url=$url."?$md5";
+        }
+        $url=$_ENV['SITE_URL'];
+        if($ext=='css'){
+            print '<link rel="stylesheet" href="'.$url.'" />';
+        }
+        if($ext=='js'){
+            print '<script src="'.$url.'"></script>';
+        }
+        print PHP_EOL;
+    }
+}
